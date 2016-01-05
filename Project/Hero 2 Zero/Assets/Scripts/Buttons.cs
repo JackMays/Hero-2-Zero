cc54431242962e874d33a094df9379ce422a6078
JackMays <maysey43@hotmail.com>
2016-01-05 15:48:52 +0000

    Main Menu Mock Up

---
+++ Project/Hero 2 Zero/Assets/Scripts/Buttons.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Buttons : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Play()
+	{
+		// Dice Rolling Scene
+		Application.LoadLevel(1);
+	}
+
+	public void Quit()
+	{
+		// exit app, ignored in editor
+		Application.Quit();
+	}
+}
