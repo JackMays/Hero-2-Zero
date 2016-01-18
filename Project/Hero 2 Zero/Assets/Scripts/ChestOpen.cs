913f7e8911b61727959911e120e80e1f4096fcd7
Zachary Huxley <TheAnimeBughead@gmail.com>
2016-01-18 00:37:09 +0000

    Chest and Skyboxes
    - Added a chest model.
    - Copied some free skyboxes from another project.

---
+++ Project/Hero 2 Zero/Assets/Scripts/ChestOpen.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChestOpen : MonoBehaviour
+{
+	#region Variables
+	// Holds whether the chest is open or not.
+	bool isOpen = false;
+	
+	// The chest's animations.
+	Animation anims;
+	#endregion
+	
+	// Use this for initialization
+	void Start ()
+	{
+		// Gets the animations.
+		anims = GetComponent<Animation>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.C)) {
+			// Checks if the chest is already open.
+			if (isOpen) {
+				// Opens the chest.
+				anims.Play("Close");
+				isOpen = false;
+			}
+			else {
+				// Closes the chest.
+				anims.Play("Open");
+				isOpen = true;
+			}
+		}
+	}
+}
