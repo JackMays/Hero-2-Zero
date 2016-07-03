885d1c9cc3225c328efb9a25ee1cf54ffe3d3372
Zachary Huxley <TheAnimeBughead@gmail.com>
2016-07-03 13:51:02 +0100

    Combat GUI
    - Set the combat GUI up so all you have to do is work it into your
    combat manager script.

---
+++ Project/Hero 2 Zero/Assets/Scripts/GUI/CombatGUI.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CombatGUI : MonoBehaviour
+{
+	#region Variables
+	// Parent of the canvas items.
+	public Transform canvasParent;
+	
+	// Holds the index of the button that was pressed.
+	int pressedButton = -1;
+	
+	// Holds the current player.
+	Player currentPlayer = null;
+	
+	#endregion
+	
+	// Returns which button was pressed.
+	public int GetPressedButton()
+	{
+		return pressedButton;
+	}	
+	
+	// Sets whether to show or hide the GUI.
+	public void ShowHideGUI(bool show)
+	{
+		canvasParent.gameObject.SetActive(show);
+	}
+	
+	// Returns whether the GUI is being shown.
+	public bool GetShowing()
+	{
+		return canvasParent.gameObject.activeSelf;
+	}
+	
+	#region Setters
+	
+	// Sets which button was pressed.
+	public void SetPressedButton(int button)
+	{
+		pressedButton = button;
+	}
+	
+	#endregion
+}
